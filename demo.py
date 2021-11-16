eaa343bc1f4ae992bc5a9f30850f20c630fe28a5
Shaun <614027488@qq.com>
2021-11-16 13:42:48 +0800

    base

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
-@app.route('/Edit')
+@app.route('/edit')
 def edit():
-    return
+    return render_template('edit.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
