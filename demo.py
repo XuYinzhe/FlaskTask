9e383cf4c461e8f171768c9c7ed54b17da0390a3
Shaun <614027488@qq.com>
2021-11-16 19:35:45 +0800

    login

--- demo.py
+++ demo.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app=Flask(__name__)
 
-@app.route('/edit')
-def edit():
-    return render_template('edit.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login',methods=['POST'])
+def _login():
+    if request.method == "POST":
+       email = request.form.get("email")
+       password = request.form.get("pass") 
+       return email + password
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
