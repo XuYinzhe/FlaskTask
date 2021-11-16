20522d7d9fb89297cd2c6cc60be45f6907eba187
Shaun <614027488@qq.com>
2021-11-16 13:36:00 +0800

    base

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
-
+'''
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
@@ -17,3 +17,23 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+'''
+
+@app.route('/')
+def hello():
+    return 'hello!'
+
+@app.route('/h1')
+def hello_h1():
+    return '<h1>hello!</h1>'
+
+@app.route('/user/<username>')
+def user(username):
+    return f'<h1>This is {username}</h1>'
+
+@app.route('/render')
+def render():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
