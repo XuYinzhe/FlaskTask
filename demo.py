235e5a29f837f888bbae07f73dcd9a07d85f8678
Shaun <614027488@qq.com>
2021-11-18 14:49:59 +0800

    login-alert

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask.helpers import url_for
+from flask import flash, get_flashed_messages
 
 app=Flask(__name__)
 
@@ -13,13 +14,34 @@ def login():
 @app.route('/login',methods=['POST'])
 def _login():
     if request.method == "POST":
-       email = request.form.get("login_email")
-       password = request.form.get("login_pass") 
-       return redirect(url_for('signin'))
+        email = request.form.get("login_email")
+        password = request.form.get("login_pass") 
+        if email=='':
+            flash('Email is empty!')
+            return render_template('login.html')
+        elif password=='':
+            flash('Password is empty!')
+            return render_template('login.html')
+        else:
+            return redirect(url_for('signin'))
 
 @app.route('/signin')
 def signin():
     return render_template('signin.html')
 
+@app.route('/signin',methods=['POST'])
+def _signin():
+    if request.method == "POST":
+        email=request.form.get("signin_email")
+        password=request.form.get('signin-pass')
+        repeat_password=request.form.get('signin-repass')
+        if password==repeat_password:
+            return 'pass'
+        else:
+            #flash('Passwords you entered are different!')
+            return render_template('signin.html')
+
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
