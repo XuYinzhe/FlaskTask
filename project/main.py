a960235d79d6050182a9b7d92a505e6cb4e5c2e3
Shaun <614027488@qq.com>
2021-11-23 23:04:44 +0800

    loading page pc

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from . import db
+import os
 
 main = Blueprint('main', __name__)  
 
